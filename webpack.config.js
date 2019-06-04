08b1b079809694eb6f839f23e449c069fba67ef7
Student <example@example.com>
2019-06-04 11:01:52 -0700

    add require statement for .env

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new UglifyJsPlugin({ sourceMap: true }),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Super Galactic Age Calculator',
+      title: 'Weather Interface',
       template: './src/index.html',
       inject: 'body'
     }),
