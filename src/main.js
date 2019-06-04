337633f78a7471cb2fa6c625c43c08c7eb1d0745
Student <example@example.com>
2019-06-04 08:53:22 -0700

    add all files and code from the lessons

--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-import { ClassName } from './class-name.js';
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
-
+// import { ClassName } from './class-name.js';
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
+//
 $(document).ready(function() {
-  let className = new ClassName;
-  return className;
+  $('#weatherLocation').click(function() {
+    let city = $('#location').val();
+    $('#location').val("");
+    $.ajax({
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9abafe55ce6c35799b79c739c11dfd1c`,
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+        $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      },
+      error: function() {
+        $('#errors').text("There was an error processing your request. Please try again.");
+      }
+    });
+  });
 });
